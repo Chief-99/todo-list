8af4afa0160a8c6c106ca71c8a7b37a16fdaa561
Similo <171865838+Chief-99@users.noreply.github.com>
2026-07-09 00:04:00 +0200

    feat:Display todo item details on DOM

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,26 @@
 import { createToDo } from "./todo.js";
 
 const addToDoButton = document.querySelector('#add-todo');
+const titleText = document.querySelector('.title');
+const descriptionText = document.querySelector('.description');
+const dueDateText = document.querySelector('.due-date');
+const priorityText = document.querySelector('.priority');
 
+const first = createToDo('Title', 'description and stuff', '2026/07/3', 'high');
 
-function domElements() {
-    addToDoButton.addEventListener('click', () => createToDo('Title', 'description and stuff', '2026/07/3', 'high'));
+function domFunctions() {
+    function addTodo() {
+        const { title, description, dueDate, priority } = first;
+        titleText.textContent += title;
+        descriptionText.textContent += description;
+        dueDateText.textContent += dueDate;
+        priorityText.textContent += priority
+    }
+
+    addToDoButton.addEventListener('click', addTodo);
+
+    return; 
 }
 
-export { domElements };
+
+export { domFunctions };
--- src/todo.js
+++ src/todo.js
@@ -2,6 +2,7 @@ import { mainList } from "./projects.js";
 
 function createToDo(title, description, dueDate, priority) {
     const todo = { title, description, dueDate, priority };
+
     mainList.push(todo);
     console.log(todo);
     return todo;
