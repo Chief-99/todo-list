5a17dd6f4292fef70203f1ea8263a1cb8e64c935
Similo <171865838+Chief-99@users.noreply.github.com>
2026-07-08 13:13:32 +0200

    add:Basic dom elements for testing

--- src/todo.js
+++ src/todo.js
@@ -1,18 +1,9 @@
-import { mainList, projects } from "./projects.js";
+import { mainList } from "./projects.js";
 
 function createToDo(title, description, dueDate, priority) {
     const todo = { title, description, dueDate, priority };
     mainList.push(todo);
     return todo;
 };
-
-const first = createToDo('Title', 'description and stuff', '2026/07/3', 'high');
-const second = createToDo('Birthday', 'The most important one in the year', '2026/10/13', 'highest');
-
-const projectList = projects();
-const test = projectList.createProject('test');
-test.todos.push(first);
-
-console.log(mainList, test.todos[0]);
 
 export { createToDo };
