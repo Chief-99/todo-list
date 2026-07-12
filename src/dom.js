74da8dd61ef08d1c71d848f4ae43462eecd0eae3
Similo <171865838+Chief-99@users.noreply.github.com>
2026-07-12 22:15:35 +0200

    add:Assign todo values from inputs to variables

--- src/dom.js
+++ src/dom.js
@@ -5,8 +5,16 @@ const titleText = document.querySelector('.title');
 const descriptionText = document.querySelector('.description');
 const dueDateText = document.querySelector('.due-date');
 const priorityText = document.querySelector('.priority');
+const titleInput = document.querySelector('#title-input');
+const descriptionInput = document.querySelector('#description-input');
+const dueDateInput = document.querySelector('#due-date-input');
+const priorityInput = document.querySelector('#priority-input');
 
 const first = createToDo('Title', 'description and stuff', '2026/07/3', 'high');
+
+function getTodoValues() {
+    
+}
 
 function domFunctions() {
     function addTodo() {
