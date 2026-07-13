6cfc37c86df9c1ec310673882d9cbfea6894bf12
Similo <171865838+Chief-99@users.noreply.github.com>
2026-07-13 16:57:07 +0200

    feat:Add todo from user inputs (no formatting or validation)

--- src/dom.js
+++ src/dom.js
@@ -1,39 +1,52 @@
 import { createToDo } from "./todo.js";
+import { mainList } from "./projects.js";
 
 const addToDoButton = document.querySelector('#add-todo');
-const titleText = document.querySelector('.title');
-const descriptionText = document.querySelector('.description');
-const dueDateText = document.querySelector('.due-date');
-const priorityText = document.querySelector('.priority');
+const form = document.getElementById('todo-form');
 const titleInput = document.querySelector('#title-input');
 const descriptionInput = document.querySelector('#description-input');
 const dueDateInput = document.querySelector('#due-date-input');
 const priorityInput = document.querySelector('#priority-input');
+const displayDiv = document.querySelector('#display-div');
 
-const first = createToDo('Title', 'description and stuff', '2026/07/3', 'high');
-
-function getTodoValues() {
+function addTodo() {
     const title = titleInput.value;
     const description = descriptionInput.value;
     const dueDate = dueDateInput.value;
     const priority = priorityInput.value;
-
     const todoObject = createToDo(title, description, dueDate, priority);
+    form.reset();
 
-    return todoObject;
+    const itemContainer = document.createElement('div');
+    displayDiv.appendChild(itemContainer);
+    console.log(mainList);
+
+    itemContainer.id = todoObject.id;
+
+    return;
+}
+
+function displayTodos(mainList) {
+    const lastTodo = mainList.at(-1);
+    const titleDisplay = document.createElement('p');
+    const descriptionDisplay = document.createElement('p');
+    const dueDateDisplay = document.createElement('p');
+    const priorityDisplay = document.createElement('p');
+    const itemContainer = document.getElementById(lastTodo.id);
+
+    titleDisplay.textContent = `Title: ${lastTodo.title}`;
+    descriptionDisplay.textContent = `Description: ${lastTodo.description}`;
+    dueDateDisplay.textContent = `Due date: ${lastTodo.dueDate}`;
+    priorityDisplay.textContent = `Priority: ${lastTodo.priority}`;
+    itemContainer.append(titleDisplay, descriptionDisplay, dueDateDisplay, priorityDisplay);
 }
 
 function domFunctions() {
-    function addTodo() {
-        const { title, description, dueDate, priority } = getTodoValues();
-        titleText.textContent += title;
-        descriptionText.textContent += description;
-        dueDateText.textContent += dueDate;
-        priorityText.textContent += priority
-    }
 
-    addToDoButton.addEventListener('click', addTodo);
-
+    addToDoButton.addEventListener('click', () => {
+        addTodo();
+        displayTodos(mainList);
+    });
     return;
 }
 
--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,8 @@
 import { mainList } from "./projects.js";
 
 function createToDo(title, description, dueDate, priority) {
-    const todo = { title, description, dueDate, priority };
+    let id = crypto.randomUUID();
+    const todo = { title, description, dueDate, priority, id };
 
     mainList.push(todo);
     console.log(todo);
